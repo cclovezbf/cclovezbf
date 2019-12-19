1d3988f7944cf86fd1bb3b00ee690f72380f9c56
cclovezbf <lovezbf1314>
2019-12-19 19:51:09 +0800

    branch1

--- lovezbf/src/main/java/GitTest.java
+++ lovezbf/src/main/java/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("master");
+        System.out.println("master-branch");
         System.out.println("branch1");
     }
 }
