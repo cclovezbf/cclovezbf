6e6b52be4ca2099fce411b985865d05ec3e8e0d5
cclovezbf <lovezbf1314>
2019-12-20 15:50:19 +0800

    master

---
+++ lovezbf/src/main/java/SparkTest.java
@@ -0,0 +1,5 @@
+public class SparkTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ lovezbf/src/main/scala/DefTest.scala
@@ -0,0 +1,8 @@
+object DefTest {
+  def main(args: Array[String]): Unit = {
+    def a = "hello"
+    val b = "hello"
+    println(a)
+    println(b)
+  }
+}
